a1820d9e162f8571f6373fa8ff6f2d8bf8081c7c
Connoroni <mitchell2k2@gmail.com>
2025-02-21 10:37:26 +0000

    added final sketches and accompanying description, made videos loop

--- sketches/interactiveSketch.js
+++ sketches/interactiveSketch.js
@@ -1,108 +1,97 @@
-let circleX;
-let circleY;
-let circleRadius;
-let circleMaximumRadius;
-let circleColor;
-let score = 0;
-let highScore;
-
+let brown1, brown2, slider;
 function setup() {
-  const interactiveCanvas = createCanvas(770, 514);
-  interactiveCanvas.parent("interactiveSketch");
-  colorMode(HSB);
-  noStroke();
-  ellipseMode(RADIUS);
-  textSize(36);
-
-  // Get the last saved high score
-  highScore = getItem("high score");
-
-  // If no score was saved, start with a value of 0
-  if (highScore === null) {
-    highScore = 0;
-  }
-}
-
-function draw() {
-  background(20);
-
-  // If the circle had not shrunk completely
-  if (circleRadius > 0) {
-    // Draw the circle
-    fill(circleColor);
-    circle(circleX, circleY, circleRadius);
-    describeElement("Circle", "Randomly colored shrinking circle");
-
-    // Shrink it
-    circleRadius -= 1;
-
-    // Show the score
-    textAlign(RIGHT, TOP);
-    fill(220);
-    text(score, width - 20, 20);
-    describeElement("Score", `Text with current score: ${score}`);
-  } else {
-    // Otherwise show the start/end screen
-    endGame();
-  }
-}
-
-function startGame() {
-  // Reset the score to 0
-  score = 0;
-
-  // Start with the circle's radius maximum at half the minimum canvas dimension
-  circleMaximumRadius = min(height / 2, width / 2);
-  resetCircle();
-}
-
-function endGame() {
-  // Pause the sketch
+  createCanvas(windowWidth, windowHeight);
+  // working in degrees not radians, slightly less performant but easier for humans to read
+  angleMode(DEGREES);
+  // prevent the draw function from being called recursively
   noLoop();
-
-  // Store the new high score
-  highScore = max(highScore, score);
-  storeItem("high score", highScore);
-
-  textAlign(CENTER, CENTER);
-  fill(220);
-  let startText = `Circle Clicker
-  Click the circle before it gets too small
-  Score: ${score}
-  High score: ${highScore}
-  Click to play`;
-  text(startText, 0, 0, width, height);
-  describeElement("Start text", `Text reading, "${startText}"`);
+  // smooth the stroke joins
+  strokeJoin(ROUND);
+  // define branch colours
+  brown1 = color("#38D360");
+  brown2 = color("#4BAF64");
+  // create a slider
+  slider = createSlider(5, 180, 5);
+  slider.position(10, 10);
+  slider.style("width", "300px");
+  // when the slider changes, redraw the fern
+  slider.input(draw);
 }
-
-function resetCircle() {
-  // Start with the circle's radius at its maximum value
-  circleRadius = circleMaximumRadius;
-  circleX = random(circleRadius, width - circleRadius);
-  circleY = random(circleRadius, height - circleRadius);
-  circleColor = color(random(240, 360), random(40, 80), random(50, 90));
+function draw() {
+  background(180);
+  // this resets all the transforms (translations and rotations)
+  resetMatrix();
+  // initially translate to the middle of the bottom edge of the canvas
+  translate(width / 2, height);
+  branch(150);
 }
-
-function mousePressed() {
-  // If the game is unpaused
-  if (isLooping() === true) {
-    // Check how far the mouse is from the circle
-    let distanceToCircle = dist(mouseX, mouseY, circleX, circleY);
-
-    // If the mouse is closer to the circle's center than the circle's radius,
-    // that means the player clicked on it
-    if (distanceToCircle < circleRadius) {
-      // Decrease the maximum radius, but don't go below 15
-      circleMaximumRadius = max(circleMaximumRadius - 1, 15);
-      resetCircle();
-
-      // Give the player a point
-      score += 1;
+function branch(len) {
+  // length of branch to draw
+  // the maximum/minimum angle of each branch
+  // value taken from the slider
+  let maxAngle = slider.value();
+  // use map() to derive branch stroke weight from current length of branch
+  strokeWeight(map(len, 10, 200, 1, 10));
+  // semi-randomly mix two colours for each branch segment
+  stroke(lerpColor(brown1, brown2, random(0.3, 0.7)));
+  // the draw command for a branch
+  line(0, 0, 0, -len);
+  // immediately translate the drawing context to the top of the line ready for the next branch call
+  translate(0, -len);
+  // recursion exit condition
+  if (len > 10) {
+    // draw leaf and terminate recursion
+    if (len < 15) {
+      console.log("Ferntest");
+      // semi-random leaf colour
+      let r = 10 + random(-20, 20);
+      let g = 160 + random(-20, 20);
+      let b = 15 + random(-20, 20);
+      fill(r, g, b, 90);
+      // semi-random leaf size
+      let size = 200 + random(40);
+      noStroke();
+    } else {
+      let arm = len * 1.1;
+      // length of arm
+      push();
+      rotate(90);
+      line(0, 0, 0, -arm);
+      let spacer = 30;
+      // space between leaves
+      let size = 18;
+      // leaf size
+      for (let i = 0; i < arm; i += spacer) {
+        let r = 10 + random(-20, 20);
+        let g = 160 + random(-20, 20);
+        let b = 15 + random(-20, 20);
+        fill(r, g, b, 100);
+        ellipse(size, i, size * 2, size);
+        ellipse(-size, i, size * 2, size);
+        //fern leaves left size
+        noStroke();
+      }
+      pop();
+      push();
+      rotate(-90);
+      line(0, 0, 0, -arm);
+      for (let i = 0; i < arm; i += spacer) {
+        let r = 10 + random(-20, 20);
+        let g = 160 + random(-20, 20);
+        let b = 15 + random(-20, 20);
+        fill(r, g, b, 100);
+        ellipse(size, i, size * 2, size);
+        ellipse(-size, i, size * 2, size);
+        //fern leaves right side
+        noStroke();
+      }
+      pop();
+      // branch 1
+      push(); // save the current drawing context
+      rotate(random(-maxAngle, maxAngle)); // rotate the whole canvas
+      // 0.8 represents the ratio of the original branch size that the new branch size should be
+      branch(len * 0.8); // create a new branch
+      pop(); // restore the drawing context
     }
-    // If the game is paused
-  } else {
-    // Start and unpause it
-    startGame();
-    loop();
   }
 }
